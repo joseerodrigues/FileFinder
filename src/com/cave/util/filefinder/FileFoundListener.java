1af2b004f7f5d8ed354d83b1807481bcd8594c7f
José Rodrigues <joseerodrigues@gmail.com>
2012-05-26 18:42:27 +0100

    first commit

---
+++ src/com/cave/util/filefinder/FileFoundListener.java
@@ -0,0 +1,12 @@
+package com.cave.util.filefinder;
+
+import java.io.File;
+
+/**
+ *
+ * @author José Rodrigues
+ */
+public interface FileFoundListener {
+
+    public void fileFound(File file);
+}
